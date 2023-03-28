b89884235f796478b0fbbd5f26b56442e5c648a2
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2023-03-28 16:22:27 +0500

    cors

--- dist/http/api-renessans.js
+++ dist/http/api-renessans.js
@@ -1,1 +1,12 @@
 "use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const server_1 = __importDefault(require("../server"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const server = http_1.default.createServer(server_1.default);
+const port = process.env.PORT || 9988;
+server.listen(port, () => console.log("Listening port on " + port));
