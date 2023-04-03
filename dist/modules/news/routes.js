1e86d2cca3ca00ca7f94e44f069bc9abb4ebb814
quvonchbek17 <quvonchbekmuysinov3@gmail.com>
2023-04-03 18:20:22 +0500

    downloadurl

--- dist/modules/news/routes.js
+++ dist/modules/news/routes.js
@@ -7,10 +7,11 @@ const news_1 = require("../../middlewares/validation/news");
 const validate_1 = __importDefault(require("../../middlewares/validation/validate"));
 const express_1 = require("express");
 const news_2 = require("./news");
+const protect_1 = __importDefault(require("../../middlewares/auth/protect"));
 const NewsRouter = (0, express_1.Router)();
 NewsRouter
     .get("/", news_2.News.Get)
-    .post("/", (0, validate_1.default)(news_1.postNews), news_2.News.Post)
-    .patch("/", (0, validate_1.default)(news_1.updateNews), news_2.News.Update)
-    .delete("/:id", (0, validate_1.default)(news_1.deleteNews, "params"), news_2.News.Delete);
+    .post("/", protect_1.default, (0, validate_1.default)(news_1.postNews), news_2.News.Post)
+    .patch("/", protect_1.default, (0, validate_1.default)(news_1.updateNews), news_2.News.Update)
+    .delete("/:id", protect_1.default, (0, validate_1.default)(news_1.deleteNews, "params"), news_2.News.Delete);
 exports.default = NewsRouter;
--- dist/modules/publications/routes.js
+++ dist/modules/publications/routes.js
@@ -7,10 +7,11 @@ const publication_1 = require("../../middlewares/validation/publication");
 const validate_1 = __importDefault(require("../../middlewares/validation/validate"));
 const express_1 = require("express");
 const publications_1 = require("./publications");
+const protect_1 = __importDefault(require("../../middlewares/auth/protect"));
 const PublicationRouter = (0, express_1.Router)();
 PublicationRouter
     .get("/", publications_1.Publications.Get)
-    .post("/", (0, validate_1.default)(publication_1.postPublication), publications_1.Publications.Post)
-    .patch("/", (0, validate_1.default)(publication_1.updatePublication), publications_1.Publications.Update)
-    .delete("/:id", (0, validate_1.default)(publication_1.deletePublication, "params"), publications_1.Publications.Delete);
+    .post("/", protect_1.default, (0, validate_1.default)(publication_1.postPublication), publications_1.Publications.Post)
+    .patch("/", protect_1.default, (0, validate_1.default)(publication_1.updatePublication), publications_1.Publications.Update)
+    .delete("/:id", protect_1.default, (0, validate_1.default)(publication_1.deletePublication, "params"), publications_1.Publications.Delete);
 exports.default = PublicationRouter;
